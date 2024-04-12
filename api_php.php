ac964d0ea83c7bff3e456fa17b2b7f4999c29483
remotephper <164305030+remotephper@users.noreply.github.com>
2024-04-12 16:07:40 +0800

    a
    a

---
+++ api_php.php
@@ -0,0 +1,52 @@
+<?php
+function sign($data, $appkey){
+    ksort($data);
+    $str = '';
+    foreach($data as $key=>$value){
+        if($key == 'sign'){
+            continue;
+        }
+        $str = $str . $key . '=' . $value . '&';
+    }
+    $str = substr($str, 0, -1);
+    if($appkey != ''){
+        $str = $str . $appkey;
+    }
+    $sign = md5($str);
+    return $sign;
+}
+$appid = '120240412140311';
+$appkey = "91c2f66448b1dca4310dca57dac75bbf";
+$data = [
+	"appid" => $appid, 
+	"now" => date('Y-m-d H:i:s'), 
+]; 
+$data['sign'] = sign($data, $appkey);
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://caipiao.usdt123.site/api/latest",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($data),
+  CURLOPT_HTTPHEADER => [
+	"content-type: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
